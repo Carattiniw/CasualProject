8e215d17f7137f6824877f61ea8a31b930a1b2e8
Will Carattini <williamcarattini@hotmail.com>
2021-09-02 18:20:44 -0400

    Will's Changes - Title Screen
    Made a basic title screen, they contain three buttons that will either start the game, show the controls, or exit the game.

---
+++ Assets/Scripts/TitleScreenMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class TitleScreenMenu : MonoBehaviour
+{
+    public string newGameScene;
+    public string backToMainMenu;
+    public string controlMenu;
+    public EventSystem ES;
+    private GameObject StoreSelected;
+
+    void Start()
+    {
+        StoreSelected = ES.firstSelectedGameObject;
+    }
+
+    void Update()
+    {
+        if (ES.currentSelectedGameObject != StoreSelected)
+        {
+            //this code stops a bug where if you accidentally click a mouse button on the pause menu while 
+            //using the controller, you would be unable to use the controller to exit the pause menu.
+            if (ES.currentSelectedGameObject == null)
+            {
+                ES.SetSelectedGameObject(StoreSelected);
+            }
+            else
+            {
+                StoreSelected = ES.currentSelectedGameObject;
+            }
+        }
+    }
+
+    public void newGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void returnToMainMenu()
+    {
+        SceneManager.LoadScene(backToMainMenu);
+    }
+
+    public void openControlsMenu()
+    {
+        SceneManager.LoadScene(controlMenu);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
