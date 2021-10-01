0cb713fa82a1d77da2dedbbac661f52b7afd8eb9
Will Carattini <williamcarattini@hotmail.com>
2021-10-01 13:37:15 -0400

    Will's Final Update
    Contains all changes made to the project before turn in.

--- Assets/Scripts/LoadNextScene.cs
+++ Assets/Scripts/LoadNextScene.cs
@@ -18,19 +18,31 @@ public class LoadNextScene : MonoBehaviour
         StoreSelected = ES.firstSelectedGameObject;
     }
 
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "ArtGallery")
+        {
+            //winScreen = 
+        }
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Player"))
         {
+            /*
             if (SceneManager.GetActiveScene().name == "ArtGallery")
             {
                 winScreen.SetActive(true);
+                //GameObject.Find("Win Screen").SetActive(true);
                 EventSystem.current.SetSelectedGameObject(GameObject.Find("Win Main Menu Button"));
                 GameManager.Instance.pause = true;
                 MusicManager.Instance.audioSource.Pause();
                 return;
             }
+            */
             SceneManager.LoadScene(loadNextLevel);
+            
         }
     }
 }
